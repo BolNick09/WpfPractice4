df4a5079c2039d868740fa990c2d3bef6ba4a51f
BolNick <BolNick09RUS@gmail.com>
2024-11-18 19:36:46 +0300

    Добавьте файлы проекта.

---
+++ WPFRormBirthDate/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFRormBirthDate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            int day = int.Parse(tbDay.Text);
+            int month = int.Parse(tbMonth.Text);
+            int year = int.Parse(tbYear.Text);
+
+            DateTime date = new DateTime(year, month, day);
+            dpBirthDate.SelectedDate = date;
+        }
+    }
+}
---
+++ WpfFormTextReading/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFormTextReading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<ProgressBar> progressBarList;
+        private List<string> fileList;
+        private static Mutex mutex = new Mutex();
+        private string fileText = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+            progressBarList = new List<ProgressBar>();
+            progressBarList.Add(pgCopyProgress1);
+
+
+            fileList = new List<string>();
+            fileList.Add("file1.txt");
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            startThreads();
+        }
+
+
+        private void Copy(string outFileName, ProgressBar progressBar)
+        {
+            byte[] buffer = new byte[8];
+            long copied = 0;
+            long length = new FileInfo(outFileName).Length;
+
+            using (Stream outStream = File.Create("copied" + outFileName))
+            using (Stream inStream = File.OpenRead(outFileName))
+            while (copied < length)
+            {
+                int read = inStream.Read(buffer, 0, buffer.Length);
+                outStream.Write(buffer, 0, read);
+                copied += read;
+                this.Dispatcher.Invoke(() => progressBar.Value = 100 * (int)(copied / length));
+            }
+        }
+
+
+        private void startThreads()
+        {
+            Thread[] threads = new Thread[1];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                int index = i;
+                threads[i] = new Thread(() => Copy(fileList[index], progressBarList[index]));
+                threads[i].Start();
+            }
+        }
+    }
+}
